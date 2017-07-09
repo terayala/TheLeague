d9939255d68a9db019b8927e0c86b4b07a36cfea
terayala <teresa.ayala@ou.edu>
2017-07-09 13:01:03 -0400

    added hibernate util

--- LeagueCommissioner/src/main/java/com/revature/daos/UserDAO.java
+++ LeagueCommissioner/src/main/java/com/revature/daos/UserDAO.java
@@ -1,5 +1,8 @@
 package com.revature.daos;
 
+import com.revature.beans.User;
+
 public interface UserDAO {
 
+	public void createCoach(User user);
 }
--- LeagueCommissioner/src/main/java/com/revature/daos/UserDAOImpl.java
+++ LeagueCommissioner/src/main/java/com/revature/daos/UserDAOImpl.java
@@ -1,5 +1,17 @@
 package com.revature.daos;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.revature.beans.User;
+import com.revature.util.HibernateUtil;
+
 public class UserDAOImpl implements UserDAO {
 
+	@Override
+	public void createCoach(User user) {
+		Session session = HibernateUtil.getSession();
+		Transaction tx = session.beginTransaction();
+		
+	}
 }
