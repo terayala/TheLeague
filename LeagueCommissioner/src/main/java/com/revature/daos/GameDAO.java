3d07f626941b10cbea8a0c96e1e15028cf980813
ddatk <dd_atk@yahoo.com>
2017-07-09 15:32:02 -0400

    GameDao getSchedule() impl

--- LeagueCommissioner/src/main/java/com/revature/daos/GameDAO.java
+++ LeagueCommissioner/src/main/java/com/revature/daos/GameDAO.java
@@ -1,5 +1,11 @@
 package com.revature.daos;
 
+import java.util.List;
+
+import com.revature.beans.Game;
+
 public interface GameDAO {
+	
+	public List<Game> getSchedule();
 
 }
--- LeagueCommissioner/src/main/java/com/revature/daos/GameDAOImpl.java
+++ LeagueCommissioner/src/main/java/com/revature/daos/GameDAOImpl.java
@@ -1,5 +1,39 @@
 package com.revature.daos;
 
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+
+import com.revature.beans.Game;
+import com.revature.util.HibernateUtil;
+
 public class GameDAOImpl implements GameDAO {
+
+	public List<Game> getSchedule() {
+		
+		List<Game> games = null;
+		Session session= HibernateUtil.getSession();
+		Transaction tx = null;
+		
+		try {
+			tx = session.beginTransaction();
+			games=session.createQuery("FROM GAMES").list(); //HQL
+		} catch (Exception e) {
+			if(tx != null){
+				tx.rollback();
+			}
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+		
+		return games;
+		
+		
+	}
+
+
 
 }
