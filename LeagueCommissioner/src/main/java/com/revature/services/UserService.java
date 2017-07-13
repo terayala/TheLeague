05c1d7c6255a03f8ccf3bcc35acf9caad076b696
Shawn Kays <shawngk@gmail.com>
2017-07-12 20:37:10 -0400

    UserService grabs User data on successful login

--- LeagueCommissioner/src/main/java/com/revature/services/UserService.java
+++ LeagueCommissioner/src/main/java/com/revature/services/UserService.java
@@ -3,17 +3,26 @@ package com.revature.services;
 import org.springframework.stereotype.Component;
 
 import com.revature.beans.User;
+import com.revature.daos.UserDAOImpl;
+import com.revature.daos.UserDAO;
 
 @Component
 public class UserService {
 	public User auth(User user) {
 		User authUser = null;
+		UserDAO userDao = new UserDAOImpl();
 		
-		if(user.getUsername().equals("admin") && user.getPassword().equals("admin")) {
-			authUser = user;
+		System.out.println("UserService invoked.");
+		System.out.println("user name: " + user.getUsername());
+		System.out.println("user password: " + user.getPassword());
+		
+		authUser = userDao.selectUserByUsername(user.getUsername());
+		
+		if(user.getUsername().equals(authUser.getUsername()) && user.getPassword().equals(authUser.getPassword())) {
+			return authUser;
+		} else {
+			return null;
 		}
-		
-		return authUser;
 		/*
 		 * This authentication will return null in all situations where
 		 * both the username and the password are not correct.
