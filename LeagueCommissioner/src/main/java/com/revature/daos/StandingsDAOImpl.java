c9a758612fa49be3cb4a6a5999be5c669de0599c
johncollin1 <innoven_john@yahoo.com>
2017-07-13 15:02:33 -0400

    Navbar

--- LeagueCommissioner/src/main/java/com/revature/daos/StandingsDAOImpl.java
+++ LeagueCommissioner/src/main/java/com/revature/daos/StandingsDAOImpl.java
@@ -31,17 +31,34 @@ public class StandingsDAOImpl implements StandingsDAO {
 		 * league standings
 		 */
 		SQLQuery query =
-			session.createSQLQuery("SELECT Teams.Team_ID AS Team_Num," +
-				"Teams.Team_Name AS Team," +
-				"SUM(Tot.Game_Played) AS Games," +
-				"SUM(Tot.Win) AS Wins," +
-				"SUM(Tot.Draw) AS Draws," +
-				"SUM(Tot.Loss) AS Losses," +
-				"" +
-				"" +
-				"" +
-				"" +
-				"" +
+			session.createSQLQuery("SELECT Teams.Team_ID AS Team_Num, " +
+				"Teams.Team_Name AS Team, " +
+				"SUM(Tot.GP) AS Games, " +
+				"SUM(Tot.Win) AS Wins, " +
+				"SUM(Tot.Draw) AS Draws, " +
+				"SUM(Tot.Loss) AS Losses, " +
+				"SUM(Tot.OT_Win) AS OT_Wins, " +
+				"SUM(Tot.OT_Loss) AS OT_Loss, " +
+				"SUM(Tot.Goals_For) AS Goals_For, " +
+				"SUM(Tot.Goals_Allowed) AS Goals_Allowed, " +
+				"SUM(Tot.Goal_Diff) AS Goal_Differential, " +
+				"SUM(Tot.Pts) AS Points " +
+				
+				"FROM (SELECT Home_Team Team," +
+				"CASE WHEN Home_Score IS NOT NULL THEN 1 ELSE 0 END GP, " +
+				"CASE WHEN Home_Score > Away_Score AND Is_Overtime = 0 THEN 1 ELSE 0 END Win, " +
+				"CASE WHEN Home_Score = Away_Score THEN 1 ELSE 0 END Draw, " +
+				"CASE WHEN Home_Score < Away_Score AND Is_Overtime = 0 THEN 1 ELSE 0 END Loss, " +
+				"CASE WHEN Home_Score > Away_Score AND Is_Overtime = 1 THEN 1 ELSE 0 END OT_Win, " +
+				"CASE WHEN Home_Score < Away_Score AND Is_Overtime = 1 THEN 1 ELSE 0 END OT_Loss, " +
+				"Home_Score Goals_For, " +
+				"Away_Score Goals_Allowed, " +
+				"(Home_Score - Games.Away_Score) Goal_Diff, " +
+				"CASE WHEN Home_Score > Away_Score AND Is_Overtime = 0 THEN " + ppw +
+				" WHEN Home_Score = Away_Score THEN " + ppt +
+				" WHEN Home_Score > Away_Score AND Is_Overtime = 1 THEN " + ppow +
+				" WHEN Home_Score < Away_Score AND Is_Overtime = 0 THEN 0 " +
+				"ELSE " + ppol + " END Pts FROM Games UNION ALL SELECT" +
 				"" +
 				"" +
 				"" +
