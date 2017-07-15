06d46b84c181150d4b144ed6f69a5124063e0b0d
Shawn Kays <shawngk@gmail.com>
2017-07-14 20:55:46 -0400

    Fixed Logout

--- LeagueCommissioner/src/main/java/com/revature/controllers/LogoutController.java
+++ LeagueCommissioner/src/main/java/com/revature/controllers/LogoutController.java
@@ -2,17 +2,28 @@ package com.revature.controllers;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.revature.beans.User;
+
 @Controller
 @RequestMapping(value = "/logout")
 public class LogoutController {
+	
+	@Autowired
+	User userTemplate;
+	
 	@RequestMapping(method = RequestMethod.GET)
 	public String logout(ModelMap modelMap, HttpSession session) {
 		session.invalidate();
+		userTemplate.setFirstName("firstname");
+		userTemplate.setLastName("lastname");
+		userTemplate.setEmail("email@email.com");
+		modelMap.addAttribute("user", userTemplate);
 		return "login";
 	}
 }
