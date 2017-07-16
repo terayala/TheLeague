05a0a78210a11df374a741060a6f04a7c98adc69
Shawn Kays <shawngk@gmail.com>
2017-07-16 13:02:20 -0400

    Added error handling to stats controller

--- LeagueCommissioner/src/main/java/com/revature/controllers/PlayerGameStatsController.java
+++ LeagueCommissioner/src/main/java/com/revature/controllers/PlayerGameStatsController.java
@@ -23,13 +23,13 @@ import com.revature.daos.UserDAOImpl;
 @Controller
 @RequestMapping(value = "/viewStats")
 public class PlayerGameStatsController {
-	
+
 	@Autowired
 	User user;
-	
+
 	@Autowired
 	Stats stat;
-	
+
 	@RequestMapping(method = RequestMethod.GET)
 	public String getPlayerGameStatsPage(ModelMap modelMap, HttpSession session) {
 		user = (User) session.getAttribute("user");
@@ -37,52 +37,52 @@ public class PlayerGameStatsController {
 		List<Stats> stats = new ArrayList<>();
 		List<List<Stats>> statsStats = new ArrayList<>();
 		List<String> names = new ArrayList<>();
-		if(user.getRole() == 1) {
+		if (user.getRole() == 1) {
 			stats = statsDao.selectStatsByPlayerId(user.getUserID());
 			modelMap.addAttribute("stats", stats);
 		}
 		UserDAO usersDao = new UserDAOImpl();
 		List<User> users = usersDao.selectUsersByTeam(user.getTeam().getTeamID());
-		
+
 		List<List<String>> aggTable = new ArrayList<>();
-		
+
 		statsStats = statsDao.selectStatsListByPlayers(users);
-		List<String> nameTemp = new ArrayList<>();
-		for(List<Stats> stats2 : statsStats) {
-			for(Stats s : stats2) {
-				nameTemp = new ArrayList<>();
-				nameTemp.add(s.getPlayer().getFirstName() + " " + s.getPlayer().getLastName());
-				aggTable.add(nameTemp);
-				break;
+		
+			List<String> nameTemp = new ArrayList<>();
+			for (List<Stats> stats2 : statsStats) {
+				for (Stats s : stats2) {
+					nameTemp = new ArrayList<>();
+					nameTemp.add(s.getPlayer().getFirstName() + " " + s.getPlayer().getLastName());
+					aggTable.add(nameTemp);
+					break;
+				}
+			}
+			if (!aggTable.isEmpty()) {
+			int aggNum = 0;
+			int i = 0;
+			List<String> allTemp = new ArrayList<>();
+			for (List<Stats> stats2 : statsStats) {
+				aggNum = 0;
+				allTemp = new ArrayList<>();
+				for (Stats s : stats2) {
+					aggNum += s.getPointsScored();
+				}
+				allTemp = aggTable.get(i);
+				allTemp.add(String.valueOf(aggNum));
+				aggTable.set(i, allTemp);
+				i++;
 			}
 		}
-		
-		int aggNum = 0;
-		int i = 0;
-		List<String> allTemp = new ArrayList<>();
-		for(List<Stats> stats2 : statsStats) {
-			aggNum = 0;
-			allTemp = new ArrayList<>();
-			for(Stats s : stats2) {
-				aggNum += s.getPointsScored();
-			}
-			allTemp = aggTable.get(i);
-			allTemp.add(String.valueOf(aggNum));
-			aggTable.set(i, allTemp);
-			i++;
-		}
-		
+
 		modelMap.addAttribute("aggTable", aggTable);
-		
+
 		return "viewStats";
 	}
-	
+
 	@RequestMapping(method = RequestMethod.POST)
-	public String doPlayerGameStats(@Valid User user, 
-								BindingResult bindingResult, 
-								ModelMap modelMap, 
-								HttpSession session) {
-		
+	public String doPlayerGameStats(@Valid User user, BindingResult bindingResult, ModelMap modelMap,
+			HttpSession session) {
+
 		return "viewStats";
 	}
 }
