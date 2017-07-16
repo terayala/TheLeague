a81f8ba675420a11b41d8a499f06dbe9985b03b4
Shawn Kays <shawngk@gmail.com>
2017-07-16 12:07:22 -0400

    Stats: Added sequence generator

--- LeagueCommissioner/src/main/java/com/revature/beans/Stats.java
+++ LeagueCommissioner/src/main/java/com/revature/beans/Stats.java
@@ -4,10 +4,13 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Index;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.Cache;
@@ -23,6 +26,8 @@ import org.springframework.stereotype.Component;
 public class Stats {
 	@Id
 	@Column(name="STATS_PK")
+	@SequenceGenerator(name = "STATSPK_SEQ", sequenceName = "STATSPK_SEQ")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "STATSPK_SEQ")
 	private int STATS_PK;
 	
 	@OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
