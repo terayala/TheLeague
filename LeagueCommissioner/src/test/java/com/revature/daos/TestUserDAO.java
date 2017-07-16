688275f34a23e4baddc0a72d1fe2358bbd37df5b
Shawn Kays <shawngk@gmail.com>
2017-07-16 18:15:55 -0400

    TestUserDAO: Now checks database

--- LeagueCommissioner/src/test/java/com/revature/daos/TestUserDAO.java
+++ LeagueCommissioner/src/test/java/com/revature/daos/TestUserDAO.java
@@ -15,6 +15,7 @@ public class TestUserDAO {
 	public void testCreateUser() {
 		User user = new User(null, "junit", "junit", 3, "junit@junit.com", "junit1928352439", "pass", 1);
 		userDao.createUser(user);
+		user = userDao.selectUserByUsername("junit1928352439");
 		assertEquals("junit1928352439", user.getUsername());
 		userDao.deleteUser(user);
 	}
