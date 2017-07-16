e0405ec0bf2c31b991ccd58d20a703d4900caf6d
Shawn Kays <shawngk@gmail.com>
2017-07-16 12:08:37 -0400

    Player/Team Stats Complete

--- LeagueCommissioner/src/main/java/com/revature/controllers/PlayerGameStatsController.java
+++ LeagueCommissioner/src/main/java/com/revature/controllers/PlayerGameStatsController.java
@@ -1,5 +1,6 @@
 package com.revature.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
@@ -16,6 +17,8 @@ import com.revature.beans.Stats;
 import com.revature.beans.User;
 import com.revature.daos.StatsDAO;
 import com.revature.daos.StatsDAOImpl;
+import com.revature.daos.UserDAO;
+import com.revature.daos.UserDAOImpl;
 
 @Controller
 @RequestMapping(value = "/viewStats")
@@ -29,12 +32,47 @@ public class PlayerGameStatsController {
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public String getPlayerGameStatsPage(ModelMap modelMap, HttpSession session) {
-		System.out.println("stats: GET");
 		user = (User) session.getAttribute("user");
 		StatsDAO statsDao = new StatsDAOImpl();
+		List<Stats> stats = new ArrayList<>();
+		List<List<Stats>> statsStats = new ArrayList<>();
+		List<String> names = new ArrayList<>();
+		if(user.getRole() == 1) {
+			stats = statsDao.selectStatsByPlayerId(user.getUserID());
+			modelMap.addAttribute("stats", stats);
+		}
+		UserDAO usersDao = new UserDAOImpl();
+		List<User> users = usersDao.selectUsersByTeam(user.getTeam().getTeamID());
 		
-		List<Stats> stats = statsDao.selectStatsByPlayerId(user.getUserID());
-		modelMap.addAttribute("stats", stats);
+		List<List<String>> aggTable = new ArrayList<>();
+		
+		statsStats = statsDao.selectStatsListByPlayers(users);
+		List<String> nameTemp = new ArrayList<>();
+		for(List<Stats> stats2 : statsStats) {
+			for(Stats s : stats2) {
+				nameTemp = new ArrayList<>();
+				nameTemp.add(s.getPlayer().getFirstName() + " " + s.getPlayer().getLastName());
+				aggTable.add(nameTemp);
+				break;
+			}
+		}
+		
+		int aggNum = 0;
+		int i = 0;
+		List<String> allTemp = new ArrayList<>();
+		for(List<Stats> stats2 : statsStats) {
+			aggNum = 0;
+			allTemp = new ArrayList<>();
+			for(Stats s : stats2) {
+				aggNum += s.getPointsScored();
+			}
+			allTemp = aggTable.get(i);
+			allTemp.add(String.valueOf(aggNum));
+			aggTable.set(i, allTemp);
+			i++;
+		}
+		
+		modelMap.addAttribute("aggTable", aggTable);
 		
 		return "viewStats";
 	}
